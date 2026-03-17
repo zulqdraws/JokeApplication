f0ab436af3cfc94ec1cccf5b326a266b282672b2
Zulqarnain Rizwan <zulqdraws@gmail.com>
2026-03-18 00:39:38 +0530

    Docs URL fixed

--- submit-service/swagger.js
+++ submit-service/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:8445",
+        url: "https://zulq-jokes-kong.centralindia.cloudapp.azure.com/",
       },
     ],
   },
